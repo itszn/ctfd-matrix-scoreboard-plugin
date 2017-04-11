ac019e6b6edce0a4afb8147c289054ad39b9f5c6
itszn <itszn@itszn.com>
2017-04-11 00:54:01 -0400

    Fix setup script

--- plugins/ctfd-matrix-scoreboard-plugin/matrix.py
+++ plugins/ctfd-matrix-scoreboard-plugin/matrix.py
@@ -4,15 +4,14 @@ from CTFd import utils, scoreboard
 ROOT = '/plugins/ctfd-matrix-scoreboard-plugin/'
 
 def load(app):
-    print "load"
 
     def scoreboard_view():
         if utils.get_config('view_scoreboard_if_authed') and not utils.authed():
             return redirect(url_for('auth.login', next=request.path))
         if utils.hide_scores():
-            return render_template('..'+ROOT+'templates/scoreboard.html', errors=['Scores are currently hidden'])
+            return render_template('scoreboard-matrix.html', errors=['Scores are currently hidden'])
         standings = scoreboard.get_standings()
-        return render_template('/..'+ROOT+'templates/scoreboard.html', teams=standings, score_frozen=utils.is_scoreboard_frozen())
+        return render_template('scoreboard-matrix.html', teams=standings, score_frozen=utils.is_scoreboard_frozen())
 
 
     app.view_functions['scoreboard.scoreboard_view']  = scoreboard_view
--- setup.sh
+++ setup.sh
@@ -1,1 +1,7 @@
+pushd ../CTFd/
+git diff CTFd/config.py > /tmp/diff
+git clean -f
+git reset --hard
+git apply /tmp/diff
+popd
 cp -r * ../CTFd/CTFd/.
