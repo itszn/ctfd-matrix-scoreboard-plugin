1023a0dfe0eca064df59d5507561cbdf4da6f192
Frank <frankli0324@hotmail.com>
2020-08-05 14:40:41 +0800

    fit into CTFd v3.0.0

--- __init__.py
+++ __init__.py
@@ -1,5 +1,3 @@
-import os
-
 from flask import (
     render_template,
     jsonify,
@@ -9,26 +7,25 @@ from flask import (
     redirect,
     request
 )
-from sqlalchemy.sql import or_
 
 from CTFd import utils, scoreboard
-from CTFd.models import db, Solves, Challenges
-from CTFd.plugins import override_template
-from CTFd.utils.config import is_scoreboard_frozen, ctf_theme, is_users_mode
+from CTFd.api.v1.challenges import ChallengeList
+from CTFd.models import db, Solves
+from CTFd.utils.config import is_scoreboard_frozen, is_users_mode
 from CTFd.utils.config.visibility import challenges_visible, scores_visible
-from CTFd.utils.dates import (
-    ctf_started, ctftime, view_after_ctf, unix_time_to_utc
-)
-from CTFd.utils.user import is_admin, authed
+from CTFd.utils.dates import unix_time_to_utc
+from CTFd.utils.decorators.visibility import check_score_visibility
+from CTFd.utils.helpers import get_infos
+from CTFd.utils.modes import get_mode_as_word, TEAMS_MODE
+from CTFd.utils.user import authed
 
 
 def load(app):
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    template_path = os.path.join(dir_path, 'scoreboard-matrix.html')
-    override_template('scoreboard.html', open(template_path).read())
-
-    matrix = Blueprint('matrix', __name__, static_folder='static')
-    app.register_blueprint(matrix, url_prefix='/matrix')
+    app.register_blueprint(Blueprint(
+        'matrix', __name__,
+        static_folder='static',
+        template_folder='templates',
+    ), url_prefix='/matrix')
 
     def get_standings():
         standings = scoreboard.get_standings()
@@ -36,7 +33,11 @@ def load(app):
         jstandings = []
         for team in standings:
             teamid = team[0]
-            solves = db.session.query(Solves.challenge_id.label('challenge_id')).filter(Solves.team_id == teamid)
+            solves = db.session.query(Solves.challenge_id.label('challenge_id'))
+            if get_mode_as_word() == TEAMS_MODE:
+                solves = solves.filter(Solves.team_id == teamid)
+            else:
+                solves = solves.filter(Solves.user_id == teamid)
             freeze = utils.get_config('freeze')
             if freeze:
                 freeze = unix_time_to_utc(freeze)
@@ -51,44 +52,27 @@ def load(app):
         return jstandings
 
     def get_challenges():
-        if not is_admin():
-            if not ctftime():
-                if view_after_ctf():
-                    pass
-                else:
-                    return []
-        if challenges_visible() and (ctf_started() or is_admin()):
-            chals = db.session.query(
-                Challenges.id,
-                Challenges.name,
-                Challenges.category
-            ).filter(or_(Challenges.state != 'hidden', Challenges.state is None)).all()
-            jchals = []
-            for x in chals:
-                jchals.append({
-                    'id': x.id,
-                    'name': x.name,
-                    'category': x.category
-                })
+        if not challenges_visible():
+            return []
+        jchals = ChallengeList.get(request.args)
+        # Sort into groups
+        categories = set(map(lambda x: x['category'], jchals['data']))
+        jchals = [j for c in categories for j in jchals['data'] if j['category'] == c]
+        return jchals
 
-            # Sort into groups
-            categories = set(map(lambda x: x['category'], jchals))
-            jchals = [j for c in categories for j in jchals if j['category'] == c]
-            return jchals
-        return []
-
+    @check_score_visibility
     def scoreboard_view():
-        if scores_visible() and not authed():
-            return redirect(url_for('auth.login', next=request.path))
-        if not scores_visible():
-            return render_template('scoreboard.html',
-                                   errors=['Scores are currently hidden'])
         standings = get_standings()
-        return render_template('scoreboard.html', standings=standings,
-                               score_frozen=is_scoreboard_frozen(),
-                               mode='users' if is_users_mode() else 'teams',
-                               challenges=get_challenges(), theme=ctf_theme())
+        infos = get_infos()
 
+        if is_scoreboard_frozen():
+            infos.append("Scoreboard has been frozen")
+
+        return render_template('scoreboard-matrix.html', standings=standings,
+                               mode='users' if is_users_mode() else 'teams',
+                               challenges=get_challenges())
+
+    @app.route('/score')
     def scores():
         json = {'standings': []}
         if scores_visible() and not authed():
@@ -104,4 +88,3 @@ def load(app):
         return jsonify(json)
 
     app.view_functions['scoreboard.listing'] = scoreboard_view
-    app.view_functions['scoreboard.score'] = scores
--- static/scoreboard-matrix.js
+++ static/scoreboard-matrix.js
@@ -1,9 +1,21 @@
-function updatescores () {
-    $.get(script_root + '/scores', function( data ) {
+const $ = CTFd.lib.$;
+
+function htmlentities(str) {
+    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+}
+
+function updatescores() {
+    $.get(CTFd.config.urlRoot + '/score', function (data) {
         teams = $.parseJSON(JSON.stringify(data));
         $('#scoreboard > tbody').empty()
         for (var i = 0; i < teams['standings'].length; i++) {
-            row = "<tr><td>{0}</td><td><a href='/team/{1}'>{2}</a></td><td>{3}</td>".format(i+1, teams['standings'][i].id, htmlentities(teams['standings'][i].team), teams['standings'][i].score);
+            row = `<tr>
+                <td>${i + 1}</td>
+                <td><a href='/${mode}/${teams['standings'][i].id}'>${
+                    htmlentities(teams['standings'][i].team)
+                }</a></td>
+                <td>${teams['standings'][i].score}</td>
+            `;
             for (var j = 0; j < challenges.length; j++) {
                 if (teams['standings'][i].solves.indexOf(challenges[j].id) != -1) {
                     row += '<td class="chalmark">✔</td>';
@@ -13,29 +25,11 @@ function updatescores () {
             }
             row += '</tr>';
             $('#scoreboard > tbody').append(row)
-        };
+        }
     });
 }
 
-function cumulativesum (arr) {
-    var result = arr.concat();
-    for (var i = 0; i < arr.length; i++){
-        result[i] = arr.slice(0, i + 1).reduce(function(p, i){ return p + i; });
-    }
-    return result
-}
-
-function UTCtoDate(utc){
-    var d = new Date(0)
-        d.setUTCSeconds(utc)
-        return d;
-}
-
-function update(){
-    updatescores();
-}
-
-setInterval(update, 300000); // Update scores every 5 minutes
+setInterval(updatescores, 300000); // Update scores every 5 minutes
 
 
 
